2c394e230f089cda949c5d0a3639d3ca02837dd2
Sean Mullan <seanmm@mchsi.com>
2014-08-01 22:10:37 -0500

    Initial Commit

---
+++ iOS Color Checker/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  iOS Color Checker
+//
+//  Created by Sean Mullan on 8/1/14.
+//  Copyright (c) 2014 SilentLupin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ iOS Color Checker/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  iOS Color Checker
+//
+//  Created by Sean Mullan on 8/1/14.
+//  Copyright (c) 2014 SilentLupin. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
